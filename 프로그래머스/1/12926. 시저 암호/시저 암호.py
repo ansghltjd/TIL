d6c08dc4ea9de9196e026a51062adbef51b5e3b7
ansghltjd <100354335+ansghltjd@users.noreply.github.com>
2024-03-06 23:15:46 +0900

    [level 1] Title: 시저 암호, Time: 9.50 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,20 @@
+def solution(s, n):
+    
+    
+    s = list(s)
+    answer = ''
+    for i in s:
+        if i == " ":
+            answer += i
+        else:
+            s = ord(i)
+            k = s
+            for _ in range(n):
+                print(k)
+                k += 1
+                if int(k) == 91:
+                    k = 65
+                if int(k) == 123:
+                    k = 97
+            answer += chr(k)
+    return answer
