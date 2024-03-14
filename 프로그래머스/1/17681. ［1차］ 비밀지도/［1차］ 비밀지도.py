69b96b94fd50503c25e15183a160c8912b9c6edb
ansghltjd <100354335+ansghltjd@users.noreply.github.com>
2024-03-14 20:13:16 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,7 @@
+def solution(n, arr1, arr2):
+    map =  [
+            bin(i | j)[2:].zfill(n).replace("1", "#").replace("0", " ")
+            for i, j in zip(arr1, arr2)
+        ]
+    answer = map
+    return answer
