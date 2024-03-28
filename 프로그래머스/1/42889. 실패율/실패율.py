729a3adede8776e5760611cf8f4d580e603420bc
HoeSeong Mun <100354335+ansghltjd@users.noreply.github.com>
2024-03-28 18:54:50 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,12 +1,11 @@
 def solution(N, stages):
-    answer = [0 for i in range(N)]
-    sta = [i for i in range(1,N+1)]
+    dic = {}
     user=len(stages)
     for i in range(1,N+1):
-        answer[i-1] = stages.count(i)/user
-        user-=stages.count(i)
-        if user == 0 :
-            break
-    zipped= sorted(zip(sta,answer),reverse=True,key = lambda x : x[1])
+        if user!=0:
+            dic[i] = stages.count(i)/user
+            user-=stages.count(i)
+        else:
+            dic[i]=0
     
-    return [i[0] for i in zipped]
+    return sorted(dic,key=lambda x: dic[x],reverse = True)
