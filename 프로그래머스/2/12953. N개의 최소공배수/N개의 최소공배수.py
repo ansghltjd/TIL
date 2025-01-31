d75f86acd0cbd76e606c04208e78f659c9799b8d
HoeSeong Mun <100354335+ansghltjd@users.noreply.github.com>
2025-01-31 20:04:56 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,9 @@
+import math
+def solution(arr):
+    
+    while len(arr) >= 2:
+        a, b = arr[0], arr[1]
+        arr.append((a*b) // math.gcd(a,b))
+        arr = arr[2:]
+        
+    return arr[0]
