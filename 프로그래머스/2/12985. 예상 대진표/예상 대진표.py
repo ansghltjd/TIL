44fb35e58121bf1398b49fb60f4e572de93b54eb
HoeSeong Mun <100354335+ansghltjd@users.noreply.github.com>
2025-03-07 15:27:43 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,12 @@
+def solution(n,a,b):
+    answer = 0
+    
+    while a!=b:
+        if a%2==1:
+            a+=1
+        a//=2
+        if b%2==1:
+            b+=1
+        b//=2
+        answer+=1
+    return answer
