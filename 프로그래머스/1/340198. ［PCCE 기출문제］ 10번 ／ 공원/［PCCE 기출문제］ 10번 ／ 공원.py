8e2cddb2cbda1cc2dcc7b9f901acbafe7eee8223
HoeSeong Mun <100354335+ansghltjd@users.noreply.github.com>
2025-05-01 12:26:35 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 6.85 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -3,11 +3,11 @@ def solution(mats, park):
     m = len(park[0])
     
     for size in sorted(mats,reverse=True):
-        for row_start in range(n-size+1):
-            for col_start in range(m-size+1):
+        for row in range(n-size+1):
+            for col in range(m-size+1):
                 place = True
-                for x in range(row_start,row_start+size):
-                    for y in range(col_start,col_start+size):
+                for x in range(row,row+size):
+                    for y in range(col,col+size):
                         if park[x][y]!='-1':
                             place=False
                             break
