40b57d51dc085d5d1aeae248e19db66f73e37f1e
HoeSeong Mun <100354335+ansghltjd@users.noreply.github.com>
2025-08-02 12:38:48 +0900

    [level 2] Title: 주식가격, Time: 30.29 ms, Memory: 18.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,11 @@
+def solution(prices):
+    answer = [0 for i in range(len(prices))]
+    stack = []
+    for idx,num in enumerate(prices):
+        while stack and prices[stack[-1]] > prices[idx]:
+            top = stack.pop()
+            answer[top] = idx - top
+        stack.append(idx)
+    for i in stack:
+        answer[i] = len(prices)-1-i
+    return answer
