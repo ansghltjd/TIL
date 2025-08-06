47225fc3490bd490786cd7505a4f4d0d132cad2c
HoeSeong Mun <100354335+ansghltjd@users.noreply.github.com>
2025-08-06 11:33:11 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,13 +1,8 @@
 def solution(skill, skill_trees):
-    answer = []
-    cnt = 0
+    answer = 0
+    
     for i in skill_trees:
-        str1 = ''
-        for j in i:
-            if j in skill:
-                str1 += j
-        answer.append(str1)
-    for i in answer:
-        if skill[:len(i)] == i:
-            cnt+=1
-    return cnt
+        s = ''.join([j for j in i if j in skill])
+        if skill[:len(s)] == s:
+            answer+=1
+    return answer
