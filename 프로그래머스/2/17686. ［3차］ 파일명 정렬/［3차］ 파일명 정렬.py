95bcdb1f9afe197770972eb809c0d3e34b14a73c
HoeSeong Mun <100354335+ansghltjd@users.noreply.github.com>
2025-08-12 12:09:40 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.76 ms, Memory: 9.74 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,22 +1,20 @@
 def solution(files):
-    parsed = []
-    for idx, f in enumerate(files):
-        n = len(f)
-        i = 0
+    answer = []
+    
+    for idx, file in enumerate(files):
+        num_start = 0
         # 1) 숫자가 시작하는 위치 찾기
-        while i < n and not f[i].isdigit():
-            i += 1
-        head = f[:i]
+        while not file[num_start].isdigit():
+            num_start += 1
+        num_end = num_start
         # 2) 숫자 부분 (연속된 숫자, 최대 5자리)
-        j = i
-        while j < n and f[j].isdigit() and (j - i) < 5:
-            j += 1
-        number = f[i:j]
-        # tail = f[j:]  # 정렬 키에는 필요 없음 (원본 반환만)
-        # 정렬용 키: (head 소문자, 숫자 정수값, 원래 인덱스)
-        parsed.append((head.lower(), int(number), idx, f))
-
+        while num_end<len(file) and file[num_end].isdigit() :
+            num_end += 1
+        number = file[num_start:num_end]
+        head = file[:num_start].lower()
+        
+        answer.append((head, int(number), idx, file))
     # HEAD(소문자) -> NUMBER(int) -> original index(안정성 확보)
-    parsed.sort(key=lambda x: (x[0], x[1], x[2]))
+    answer.sort(key = lambda x : (x[0], x[1], x[2]))
 
-    return [t[3] for t in parsed]
+    return [i[3] for i in answer]
