dffe082bfce7b267c9490cafb81e7e627cebc27e
HoeSeong Mun <100354335+ansghltjd@users.noreply.github.com>
2025-09-01 14:28:32 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 99.07 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -3,18 +3,17 @@ def solution(files):
     
     for idx, file in enumerate(files):
         num_start = 0
-        # 1) 숫자가 시작하는 위치 찾기
         while not file[num_start].isdigit():
             num_start += 1
-        num_end = num_start
-        # 2) 숫자 부분 (연속된 숫자, 최대 5자리)
-        while num_end<len(file) and file[num_end].isdigit() :
-            num_end += 1
-        number = file[num_start:num_end]
-        head = file[:num_start].lower()
         
-        answer.append((head, int(number), idx, file))
-    # HEAD(소문자) -> NUMBER(int) -> original index(안정성 확보)
-    answer.sort(key = lambda x : (x[0], x[1], x[2]))
-
+        num_end = num_start
+        
+        while num_end < len(file) and file[num_end].isdigit():
+            num_end += 1
+            
+        head = file[:num_start].lower()
+        num = int(file[num_start:num_end])
+        
+        answer.append((head,num,idx,file))
+        answer.sort(key = lambda x:(x[0], x[1], x[2]))
     return [i[3] for i in answer]
